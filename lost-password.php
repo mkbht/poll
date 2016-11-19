9a0c216bde5a70166f7ec949cce9e6fe7e732f3e
Mukunda Bhattarai <mkdbht@gmail.com>
2016-11-19 10:17:32 +0545

    fixes

--- lost-password.php
+++ lost-password.php
@@ -38,8 +38,7 @@ if(isset($_POST['submit'])) {
 		// 			<br>
 		// 			Regards ".siteName();
 		mail($to, $subject, $message, $headers);
-		$msg = $message;
-		//$msg = "A reset link has been sent to email. Follow the link to reset your password.";
+		$msg = "A reset link has been sent to email. Follow the link to reset your password.";
 	}
 	else {
 		"<b class='text-danger'>Unknown error occurred.</b>";
