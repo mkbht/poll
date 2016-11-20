6980b23e51986e9d3d6c338185aaae1efbe83bdf
Mukunda Bhattarai <mkdbht@gmail.com>
2016-11-20 16:01:10 +0545

    pages added

--- admin/editSite.php
+++ admin/editSite.php
@@ -5,8 +5,11 @@ if(isset($_POST["submit"])) {
 	$description = htmlspecialchars($_POST["description"]);
 	$ad1 = $_POST["ad1"];
 	$ad2 = $_POST["ad2"];
+	$about = $_POST["about"];
+	$privacy = $_POST["privacy"];
+	$contact = $_POST["contact"];
 	$stmt = $c->prepare("UPDATE site_details SET sitename=?,description=?, ad1=?, ad2=?");
-	$stmt->bind_param('ssss', $sitename, $description, $ad1, $ad2);
+	$stmt->bind_param('sssssss', $sitename, $description, $ad1, $ad2, $about, $privacy, $contact);
 	$stmt->execute();
 	$msg = "Updated successfully";
 }
@@ -25,6 +28,13 @@ include 'inc/header.php';
 	<textarea name="ad1" class="form-control"><?=siteDetails()->ad1?></textarea>
 	<label>Ad code 2</label>
 	<textarea name="ad2" class="form-control"><?=siteDetails()->ad2?></textarea>
+	<!-- about -->
+	<label>About</label>
+	<textarea name="about" class="form-control" rows="20"><?=siteDetails()->about?></textarea>
+	<label>Privacy Policy</label>
+	<textarea name="privacy" class="form-control" rows="20"><?=siteDetails()->about?></textarea>
+	<label>Contact</label>
+	<textarea name="contact" class="form-control" rows="20"><?=siteDetails()->about?></textarea>
 	<br>
 	<button type="submit" name="submit" class="btn btn-inverse">Update</button>
 </form>
