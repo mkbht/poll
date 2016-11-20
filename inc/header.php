7faaa18fa93f6f79277b08ae6882ea96988ea36c
root <root@zorgan.me>
2016-11-20 09:59:03 +0000

    css fixes

--- inc/header.php
+++ inc/header.php
@@ -4,6 +4,13 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-1488457803116707",
+    enable_page_level_ads: true
+  });
+</script>
     <meta charset="utf-8">
     <title><?=$sitename?> <?=isset($title)?' - '.$title:'';?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
